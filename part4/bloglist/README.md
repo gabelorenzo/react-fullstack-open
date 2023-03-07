121b8c7b6b4f0f92260c280dcff1b4c923cb02e9
Gabe Lorenzo <gabe.lorenzo@washpost.com>
2023-03-07 16:40:38 -0500

    Part 4 - Exercise 4.2

--- part4/bloglist/README.md
+++ part4/bloglist/README.md
@@ -1,9 +1,21 @@
 # Setup
 
-w/ Local Database
+### Prerequisites
+
+1. `Docker` is installed so MongoDB can be setup on your machine.
+2. `nvm` is installed to manage node/npm version.
 
 ```bash
     docker pull mongo
     docker run -d -p 27017:27017 --name test-mongo mongo:latest
+    nvm install
+    nvm use
+```
+
+### Run locally
+
+```bash
     npm run dev
 ```
+
+Make requests to [localhost:3001](http://localhost:3003/api/blogs)
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,23 +1,19 @@
-const http = require("http");
 const express = require("express");
 const app = express();
 const cors = require("cors");
 const mongoose = require("mongoose");
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
+const Blog = require("./models/blog");
+const morgan = require("morgan");
 
 const mongoUrl = "mongodb://localhost/bloglist";
 mongoose.connect(mongoUrl);
 
+// Configure Morgan request logger
+morgan.token('body', (req) => JSON.stringify(req.body))
+// Needed to parse the request body as JSON
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+app.use(express.json())
 app.use(cors());
-app.use(express.json());
 
 app.get("/api/blogs", (request, response) => {
   Blog.find({}).then((blogs) => {
@@ -27,6 +23,8 @@ app.get("/api/blogs", (request, response) => {
 
 app.post("/api/blogs", (request, response) => {
   const blog = new Blog(request.body);
+
+  console.log("BLOG" , request.body)
 
   blog.save().then((result) => {
     response.status(201).json(result);
